98915a4a3e3f530ce8040597ea92dca1b229c9b8
Clément METZ <metz.clement@gmail.com>
2026-05-03 18:05:05 +0200

    feat(libai): defined libai::providers::AIProvider abstract class

---
+++ libs/libai/include/libai/providers/ai_provider.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <curl/curl.h>
+#include <string>
+#include <vector>
+
+namespace libai::providers {
+
+    class AIProvider {
+       public:
+        AIProvider(const std::string& name, const std::string& root_url,
+                   const std::string& api_key);
+
+        const std::string& getName() const;
+        const std::string& getRootUrl() const;
+        const std::string& getApiKey() const;
+        const int getMaxTokens() const;
+
+        virtual std::vector<std::string> getModels() const = 0;
+        virtual const std::string generateSingleResponse(
+            const std::string& model_name, const std::string& prompt) const = 0;
+
+       protected:
+        virtual curl_slist* getBaseHeaders() const = 0;
+
+        const std::string name_;
+        const std::string root_url_;
+        const std::string api_key_;
+        const int max_tokens_ = 1024;
+    };
+
+}  // namespace libai::providers
